1b9c8d95f9e1454279c5e952eafb4bfcfffe977e
James Graham <james@hoppipolla.co.uk>
2017-06-30 11:47:32 -0700

    Example unstable test

--- tools/ci/check_stability.py
+++ tools/ci/check_stability.py
@@ -11,7 +11,7 @@ wpt_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os
 sys.path.insert(0, wpt_root)
 
 from tools.wpt import testfiles
-from testfiles import get_git_cmd
+from tools.wpt.testfiles import get_git_cmd
 from tools.browserutils.virtualenv import Virtualenv
 from tools.browserutils.utils import Kwargs
 from tools.wpt.run import create_parser, setup_wptrunner
